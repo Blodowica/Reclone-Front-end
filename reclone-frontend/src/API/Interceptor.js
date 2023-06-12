62e051f83e4d20f6c1064cc0f4b3981986464060
Brandon Lodowica <brandonlodowica@hotmail.com>
2023-06-12 09:57:06 +0200

    Update Interceptor.js

--- reclone-frontend/src/API/Interceptor.js
+++ reclone-frontend/src/API/Interceptor.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7041/api/', // Update with your API URL
+    baseURL: process.env.REACT_APP_API, // Update with your API URL
 });
 
 api.interceptors.request.use(
